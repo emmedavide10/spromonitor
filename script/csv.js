f437d2643dc218e8cdac2f92ea91022f4dc6a66c
git.user <davide.mirra@iss.it>
2024-01-17 14:38:20 +0100

    fix comments

--- script/csv.js
+++ script/csv.js
@@ -1,27 +1,27 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-    // Funzione per nascondere l'errore dopo un certo periodo di tempo
+    // Function to hide the error message after a certain period of time
     function hideErrorMessage() {
         var errorFile = document.getElementById('error-file');
         errorFile.style.display = 'none';
 
-        // Torna indietro di una pagina solo se non ci sono errori di JavaScript
+        // Go back one page only if there are no JavaScript errors
         if (!window.jsError) {
-            // Utilizza replaceState per evitare che la pagina corrente venga aggiunta alla cronologia
+            // Use replaceState to prevent the current page from being added to the browser history
             history.replaceState(null, document.title, location.href);
-            history.back(); // Torna indietro di una pagina
+            history.back(); // Go back one page
         }
     }
 
-    // Nascondi il messaggio dopo 2 secondi (2000 millisecondi)
+    // Hide the message after 2 seconds (2000 milliseconds)
     setTimeout(hideErrorMessage, 2000);
     var csvButton = document.getElementById('csv');
     var downloadSection = document.getElementById('downloadSection');
 
+    // Event listener for the CSV button
     csvButton.addEventListener('click', function (event) {
         event.preventDefault(); // Prevent the default form submission behavior
-        document.getElementById('csvbutton').style.display = 'none'; // Hide the csv generate button 
+        document.getElementById('csvbutton').style.display = 'none'; // Hide the CSV generate button 
         downloadSection.style.display = 'block'; // Show the download section
     });
 });
-
